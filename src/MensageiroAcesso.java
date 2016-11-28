a9b0220c17333540807bb5e43a900cb90b31f2e6
voce <voce@voce-Aspire>
2016-11-28 16:41:06 -0200

    Te2

--- src/MensageiroAcesso.java
+++ src/MensageiroAcesso.java
@@ -1,7 +1,14 @@
 public interface MensageiroAcesso {
-
+    /**
+     * Entra na conta administrador
+     * @param login Login da conta
+     * @param senha Senha da conta
+     * @return true, caso sucesso<br/>false, caso contrário
+     */
     public abstract boolean entrar(String login, String senha);
-
+    /**
+     * Sai da conta administrador
+     * @return true, caso sucesso<br/>false, caso contrário
+     */
     public abstract boolean sair();
-
 }
